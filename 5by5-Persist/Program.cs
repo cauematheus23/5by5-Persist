bc958f7e7a57ca574d366f4d6c9431d181cf5fac
cauematheus <cauematheus_23@hotmail.com>
2024-06-03 00:45:14 -0300

    Adicionar arquivos de projeto.

---
+++ 5by5-Persist/Program.cs
@@ -0,0 +1,7 @@
+﻿using Controller;
+using Model;
+
+string path = "C:\\Users\\cauem\\JSONS\\dados_dos_radares.json";
+var lst = ReadFile.GetData(path);
+
+PersistController pc = new PersistController();
---
+++ Controller/PersistController.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using Service;
+
+namespace Controller
+{
+    public class PersistController
+    {
+        private PersistService _service;
+        public PersistController()
+        {
+            _service = new PersistService();
+        }
+        public bool Insert(List<Infracao> infracoes)
+        {
+
+            return _service.Insert(infracoes);
+        }
+        public List<Infracao> GetAll() { return _service.GetAll(); }
+    }
+}
